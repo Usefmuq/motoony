c26642b23caec0e3e9eb4e9a85a2ba7c4ff93eb3
usefmuq <usefmuq@gmail.com>
2021-02-01 17:15:38 +0300

    started content_editor

--- classes/contentview_class.php
+++ classes/contentview_class.php
@@ -11,7 +11,7 @@ class ContentView extends Content {
     public function add_button()
     {
       if ($this->is_authorized()){
-        echo '<a href="submit.php?add_content=true&content_section='.$_GET['id'].'" class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-plus"></i> ADD CONTENT</a>';
+        echo '<a href="content_editor.php?id='.$_GET['id'].'" class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-plus"></i> ADD CONTENT</a>';
       }
     }
     public function show_content()
@@ -42,12 +42,51 @@ class ContentView extends Content {
             }
             echo '</tbody>
             </table>
-            </div>';
+            </div> <p>';
             foreach ($content_result as $row){
-            echo '<p id="case-'.$row ["content_case"].'">'.$row ["content_content"].'</p>';
+            echo ' '.$row ["content_content"];
             }
-            echo '';
+            echo '</p>';
+    }
+    public function show_editor()
+    {
+      $content_result = $this->get_content($_GET['id']);
+      $cases_result = $this->get_cases($_GET['id']);
+      foreach ($content_result as $row){
+        echo '
+        <div class="card">
+        <div class="card-header">
+        '.$row ["content_content"].'
+        </div>
+        <div class="card-body">
+        <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Case Name</th>
+            <th>Go</th>
+            <th>Case link</th>
+          </tr>
+        </thead>
+        <tbody>';
+        foreach ($cases_result as $row){
+            echo '
+          <tr>
+            <td>'.$row ["case_id"].'</td>
+            <td><a href="#case-'.$row ["case_id"].'">'.$row ["case_name"].'</a></td>
+            <td><button type="button" class="btn btn-warning" onclick="case_search(\'cupidatat nulla elit cupidatat\')">عرض</button></td>
+            <td><button type="button" class="btn btn-info">الشرح والتعليقات</button></td></td>
+          </tr>';
+        }
+        echo '</tbody>
+        </table>
+        </div>
+        </div>
+        <br>';
+
+
+      }
+
     }
 
 }
-//'.$row ["content_content"].'
